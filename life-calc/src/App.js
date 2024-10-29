f985053321a3d16e03c96234cc7a94da500714ea
zarif3624 <zarif.choudhury@utexas.edu>
2024-10-29 13:46:02 +0700

    retrying app

---
+++ life-calc/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, GitHub Pages!</h1>
+        <p>This is my first deployed React app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
